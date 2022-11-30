8e86a72c5dda5b26a28cc07ef0007828a64a036b
Dominic Skinner <dominic@dominics-mbp.ads.northwestern.edu>
2022-11-30 15:35:01 -0600

    updated precompilation

--- compiled/make.jl
+++ compiled/make.jl
@@ -2,8 +2,13 @@ using PackageCompiler
 
 include("packages.jl")
 
-PackageCompiler.create_sysimage(
-  PACKAGES,
-  sysimage_path = "compiled/sysimg.so",
-  cpu_target = PackageCompiler.default_app_cpu_target()
-)
+function make()
+  PackageCompiler.create_sysimage(
+    PACKAGES,
+    sysimage_path = "compiled/sysimg.so",
+    precompile_execution_file = "compiled/precompile.jl",
+    cpu_target = PackageCompiler.default_app_cpu_target()
+  )
+end
+
+make()
